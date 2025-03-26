03fcfe5d3209947eecd6224943af6512191aebae
Trent Vander Veen <trentvanderveen@zohomail.com>
2025-03-25 21:39:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,33 @@
-let todos = JSON.parse(localStorage.getItem("todos")) || ["Add your first todo!"] // Sets a default value if localstorage value is equal to null
-const list = document.getElementById("todoList");
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
 
-function updateList(newTodo) {
-    if (newTodo !== "") {
-        const todoElement = document.createElement("li");
-        todoElement.innerHTML = `${newTodo}<button class="removeButton">X</button>`;
-        list.appendChild(todoElement);
+function addTask(){
+    if (inputBox.value === ''){
+        alert("You must write something!")
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = "";
+    saveData();
 }
 
-function updateLocalStorage() {
-    localStorage.setItem("todos", JSON.stringify(todos))
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+})
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML)
 }
 
-function addTodo() {
-    const todoInput = prompt("Add to the list: ");
-    if (todoInput) {
-        todos.push(todoInput);
-        updateList(todoInput);
-        updateLocalStorage()
-    }
-}
-
-list.addEventListener("click", (event) => { // Remove button listener
-    if (event.target.classList.contains("removeButton")) {
-        const todoElement = event.target.parentElement
-        const todoText = todoElement.textContent.slice(0, -1) // Remove the last letter X in the string
-        todos = todos.filter(todo => todo !== todoText); // if false then todo is removed if true todo is kept
-        list.removeChild(todoElement)
-        updateLocalStorage()
-    }
-});
-
-todos.forEach(updateList);
-
-document.getElementById("addButton").addEventListener("click", addTodo);
+listContainer.innerHTML = localStorage.getItem("data");
